55816ed0a4399be946f7f09eef7bf8fde230d7e7
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-07-05 19:18:59 +0300

    Designing collection view cell

---
+++ Design+Code/Design+Code/HandbookCollectionViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  HandbookCollectionViewCell.swift
+//  Design+Code
+//
+//  Created by Volodymyr Myroniuk on 05.07.2023.
+//
+
+import UIKit
+
+final class HandbookCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var overlay: UIView!
+    @IBOutlet weak var banner: UIImageView!
+    @IBOutlet weak var logo: CustomImageView!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    let gradient = CAGradientLayer()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(named: "Shadow")?.cgColor
+        layer.shadowOpacity = 0.25
+        layer.shadowOffset = CGSize(width: 0, height: 5)
+        layer.shadowRadius = 10
+        layer.masksToBounds = false
+        layer.cornerRadius = 30
+        layer.cornerCurve = .continuous
+        
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        gradient.frame = overlay.frame
+        gradient.cornerCurve = .continuous
+        gradient.cornerRadius = 30
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        super.layoutIfNeeded()
+    }
+    
+}
--- Design+Code/Design+Code/ViewController.swift
+++ Design+Code/Design+Code/ViewController.swift
@@ -7,10 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private weak var cardView: CustomView!
+    @IBOutlet private weak var handbooksCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        handbooksCollectionView.delegate = self
+        handbooksCollectionView.dataSource = self
         
 //        cardView.layer.cornerCurve = .continuous
 //        cardView.layer.cornerRadius = 30
@@ -26,3 +32,25 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UICollectionViewDelegate {}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CourseCell", for: indexPath) as! HandbookCollectionViewCell
+        
+        cell.titleLabel.text = "SwiftUI Handbook"
+        cell.subtitleLabel.text = "20 SECTIONS - 3 HOURS"
+        cell.descriptionLabel.text = "Learn about all the basics of SwiftUI"
+        cell.gradient.colors = [UIColor.red.cgColor, UIColor.systemPink.cgColor]
+        cell.logo.image = UIImage(named: "Logo React")
+        cell.banner.image = UIImage(named: "Illustration 2")
+        
+        return cell
+    }
+    
+}
