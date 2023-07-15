08444a56ca2487be93f44d7676c246208473df0b
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-07-15 06:02:10 +0300

    Populate table view inside Courses View Controller

--- Design+Code/Design+Code/View Controllers/CoursesViewController.swift
+++ Design+Code/Design+Code/View Controllers/CoursesViewController.swift
@@ -6,25 +6,76 @@
 //
 
 import UIKit
+import Combine
 
 class CoursesViewController: UIViewController {
+    
+    @IBOutlet var bannerImage: UIImageView!
+    @IBOutlet var backgroundImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var authorLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet weak var sectionsTableView: UITableView!
+    @IBOutlet weak var tableViewHeight: NSLayoutConstraint!
+    
+    private var tokens: Set<AnyCancellable> = []
     
     var course: Course?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("This course is \(course?.title ?? "")")
+        
+        sectionsTableView.delegate = self
+        sectionsTableView.dataSource = self
+        sectionsTableView.publisher(for: \.contentSize)
+            .sink { newContentSize in
+                self.tableViewHeight.constant = newContentSize.height
+            }
+            .store(in: &tokens)
+        sectionsTableView.rowHeight = UITableView.automaticDimension
+        sectionsTableView.estimatedRowHeight = UITableView.automaticDimension
+        
+        self.bannerImage.image = course?.banner
+        self.backgroundImage.image = course?.background
+        self.titleLabel.text = course?.title
+        self.subtitleLabel.text = course?.subtitle
+        self.descriptionLabel.text = course?.description
+        self.authorLabel.text = "Taught by \(course?.authors?.formatted(.list(type: .and, width: .standard)) ?? "Design+Code")"
+    }
+
+    @IBAction func goBack(_ sender: UIButton) {
+        dismiss(animated: true)
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension CoursesViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        UITableView.automaticDimension
+    }
+}
 
+extension CoursesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        course?.sections.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SectionCell", for: indexPath) as! SectionTableViewCell
+        if let selectedSection = course?.sections[indexPath.row] {
+            cell.titleLabel.text = selectedSection.sectionTitle
+            cell.subtitleLabel.text = selectedSection.sectionSubtitle
+            cell.descriptionLabel.text = selectedSection.sectionDescription
+            cell.courseLogo.image = selectedSection.sectionIcon
+        }
+        return cell
+    }
+    
 }
