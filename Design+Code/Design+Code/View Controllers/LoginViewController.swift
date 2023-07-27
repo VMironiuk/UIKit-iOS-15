f0eb145326742c89d9be8cd6f543d1dcf795bb64
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-07-27 19:17:44 +0300

    Firebase auth

--- Design+Code/Design+Code/View Controllers/LoginViewController.swift
+++ Design+Code/Design+Code/View Controllers/LoginViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Combine
 
 class LoginViewController: UIViewController {
     @IBOutlet var loginCard: CustomView!
@@ -15,6 +17,10 @@ class LoginViewController: UIViewController {
     @IBOutlet var emailTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
     
+    var emailIsEmpty = true
+    var passwordIsEmpty = true
+    private var tokens: Set<AnyCancellable> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,5 +28,44 @@ class LoginViewController: UIViewController {
             self.loginCard.alpha = 1
             self.loginCard.frame = self.loginCard.frame.offsetBy(dx: 0, dy: -400)
         }
+        
+        emailTextfield.publisher(for: \.text)
+            .sink { newValue in
+                self.emailIsEmpty = (newValue == "" || newValue == nil)
+            }
+            .store(in: &tokens)
+        
+        passwordTextfield.publisher(for: \.text)
+            .sink { newValue in
+                self.passwordIsEmpty = (newValue == "" || newValue == nil)
+            }
+            .store(in: &tokens)
+    }
+    
+    private func goToHomeScreen() {
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CustomTabBarViewController") as! CustomTabBarViewController
+        vc.modalTransitionStyle = .flipHorizontal
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    @IBAction private func primaryButtonAction(_ sender: UIButton) {
+        if (emailIsEmpty || passwordIsEmpty) {
+            let alert = UIAlertController(title: "Missing Information", message: "Please make sure to enter a valid email address and a password", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { authResult, error in
+                guard error == nil else {
+                    print(error!.localizedDescription)
+                    return
+                }
+                
+                self.goToHomeScreen()
+            }
+        }
+    }
+    
+    @IBAction private func accessoryButtonAction(_ sender: UIButton) {
     }
 }
